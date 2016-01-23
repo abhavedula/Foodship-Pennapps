24e94778a5dbe8e9f7ae4191b7769fe7b4f93ba4
Victoria Huang <vichuang@seas.upenn.edu>
2016-01-23 16:02:58 -0500

    updated makeDelivery

--- makeDelivery.rb
+++ makeDelivery.rb
@@ -32,10 +32,31 @@ def createDelivery(pickup_address, pickup_name, pickup_phone_number, dropoff_add
 	request["authorization"] = 'Basic MGI1ODYyNjItNjQ4OS00Y2U4LTlhMzUtYmU4ZmIwZTc0NTE2Og=='
 	request["cache-control"] = 'no-cache'
 	request["postman-token"] = 'e307a6c0-64f5-8d26-261a-a201ece9aeaf'
-	request.body = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"pickup_address\"\r\n\r\n"+pickup_address+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"pickup_phone_number\"\r\n\r\n"+pickup_phone_number+
-	"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"pickup_name\"\r\n\r\n"+pickup_name+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dropoff_address\"\r\n\r\n"+dropoff_address+
-	"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"manifest\"\r\n\r\n"+manifest+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dropoff_phone_number\"\r\n\r\n"+dropoff_phone_number+
-	"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dropoff_name\"\r\n\r\n"+dropoff_name+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"quote_id\"\r\n\r\n"+quote_id+"\r\n-----011000010111000001101001--"
+
+	url = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"pickup_address\"\r\n\r\n"+pickup_address+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dropoff_address\"\r\n\r\n"+dropoff_address+"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"quote_id\"\r\n\r\n"+quote_id
+
+	if pickup_name != null
+		url += "\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"pickup_name\"\r\n\r\n"+pickup_name
+	end
+	if pickup_phone_number != null
+		url += "\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"pickup_phone_number\"\r\n\r\n"+pickup_phone_number
+	end
+	if manifest != null
+		"\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"manifest\"\r\n\r\n"+manifest
+	end
+	if dropoff_address != null
+		url += "\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dropoff_address\"\r\n\r\n"+dropoff_address
+	end
+	if dropoff_phone_number != null
+		url += "\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dropoff_phone_number\"\r\n\r\n"+dropoff_phone_number
+	end
+	if dropoff_name != null
+		url += "\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"dropoff_name\"\r\n\r\n"+dropoff_name
+	end
+
+	url += "\r\n-----011000010111000001101001--"
+
+	request.body = url
 
 	response = http.request(request)
 	puts response.read_body
