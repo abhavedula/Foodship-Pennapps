b9d400e396ce940771b715e8ac9a00bf84dd4963
Victoria Huang <vichuang@seas.upenn.edu>
2016-01-23 19:53:04 -0500

    uploaded everyblockInfo

---
+++ everyblockInfo.js
@@ -0,0 +1,11 @@
+function getInfo(text) {
+	var token = "MTH9u1Mmj4Wzhjn";
+	var xhr = new XMLHttpRequest();
+
+	var metro = "philly";
+              var schema = "business%20reviews";
+
+              var endpoint = "https://api.everyblock.com/content/" + "?token=";
+	xhr.open("GET", endpoint + token + "&metro=" + metro + "&schema=" + schema"; false);
+	xhr.send();
+}
